38c5518dab8b6308adffd2a1d77441e7f3b54efd
RapidFire3 <jgardnerjr14@yahoo.com>
2025-03-02 13:48:19 +0000

    Updated MSB File
    -	Modified this program to locate the Most Significant Bit of a 32-bit integer to a 64-bit unsigned integer.
    -	Implemented fixed-width integers for guaranteed range.

--- src/MostSignificantBit.cpp
+++ src/MostSignificantBit.cpp
@@ -1,75 +1,87 @@
-#include<iostream>
-#include<sstream>
+#include <iostream>
+#include <sstream>
+#include <cstdint>
 
 /*******************************************************************************
  *                          getMostSignificantBit()
- * This method is responsible for returning the index or position of the most
- * significant bit of a given integer when in the binary representation.
+ * 	
+ *	This function is responsible for returning the index or position of the most
+ * 	significant bit of a given integer when in the binary representation.
  * 
- * @param inputNumber   the number that the method must find the msb of.
+ * @param inputNumber   the 64 bit signed integer that the function must locate 
+ *						the MSB for.
  * @return              the most significant bit of the input number.
  * @author              Jeff Gardner
 ********************************************************************************/
-int getMostSignificantBit(int inputNumber)
+int getMostSignificantBit( int64_t inputNumber )
 {
     //  Number of bits of the input number that must tested for the potential
     //  most significant bit.
-    int bitSize = 32;
+    int bitSize{ 64 };
 
     //  Current bit index starting from most significant bit to least significant 
-    //  bit (left -> right) of the input number.
-    int currentIndex = 0;
+    //  bit ( left -> right ) of the input number.
+    int currentIndex{ 0 };
 
     //  Index of the most significant bit.
-    int msbIndex = 0; 
+    int msbIndex{ 0 };
 
-    if (inputNumber != 0) 
+
+    if ( inputNumber != 0 ) 
     {
-        while (currentIndex < bitSize)
-        {
+        while ( currentIndex < bitSize ) 
+		{
             //  The first index that is set or not zero is the most significant bit.
             //  The current index is the MSB and will break out of while loop.
-            if ((inputNumber & 0x80000000) == 0x80000000)
-                break;
+            if (( inputNumber & 0x8000000000000000 ) == 0x8000000000000000 )
+            	break;
 
             //  Conduct a left shift by 1 to determine if the next index is a potential
             //  MSB.
             inputNumber <<= 1;
-
+			
             currentIndex++;
         }
 
         //  The index or the position of the most significant bit of the input number.
-        //  From least to most significant bit (right -> left)
-        msbIndex = bitSize - (currentIndex + 1);
+        //  From least to most significant bit ( right -> left )
+        msbIndex = bitSize - ( currentIndex + 1 );
     }
 
     return msbIndex;
 }
 
 
-int main(int argc, char* argv[]) {
-    if(argc < 2) 
-        std::cout << "Please provide a valid Integer."<< std::endl;
-    
+int main( int argc, char* argv[] ) 
+{
+	//	Ensure that a value was provided with the file name.
+    if( argc < 2 ) 
+	{
+        std::cout << "Please provide a valid Integer. \n";
+    }
+
     else 
     {
-        //  Holds the string version of the integer.
+        //	Will contain the integer argument passed to the program as a string.
         std::stringstream inArg;
 
-        //  Put the argument onto the buffer to prepare for conversion.
-        inArg.str(argv[1]);
+        //  Place the argument, which should be the desired integer,  onto the 
+		//	buffer to prepare for string to integer conversion.
+        inArg.str( argv[1] );
 
-        //  Extract the buffer contents to integer variable.
-        int usrValue = 0;
-        inArg >> usrValue;
+        //  Extract the stringstream buffer contents to an integer type.
+        int64_t valueToProcess{};
+        inArg >> valueToProcess;
 
-        //  Clear the buffer.
-        inArg.str("");
+		//  Clear the buffer.
+        inArg.str( "" );
 
-        //  Find the most significant bit if the value is not out of bounds.
-        if (usrValue < INT32_MAX && usrValue > INT32_MIN )
-            std::cout << getMostSignificantBit(usrValue) << std::endl;
+        //	Locate the position of the Most Significant Bit ( MSB ) of a 32-bit 
+		//	value passed to the program.
+		if ( valueToProcess <= INT64_MAX && valueToProcess >= INT64_MIN )
+        {   
+			std::cout << getMostSignificantBit( valueToProcess ) << '\n';
+		}
     }
     
     return 0;
